32a4922ab96c984acb05b0977bf655b99fdb7ead
programmingflo <flori08mans@gmail.com>
2017-12-20 12:54:49 +0100

    complete command execution

--- src/programming146/fernsteuerung/java/CommandExecution.java
+++ src/programming146/fernsteuerung/java/CommandExecution.java
@@ -4,24 +4,49 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.awt.*;
-import java.awt.event.KeyEvent.*;
 
-import static java.awt.event.KeyEvent.VK_T;
-import static java.awt.event.KeyEvent.VK_WINDOWS;
+import static java.awt.event.KeyEvent.*;
 
 
 public class CommandExecution {
-    public CommandExecution(JSONObject rawCommand) throws AWTException {
+    CommandExecution() {
     }
 
-    public String executeCommand(JSONObject rawCommand) throws JSONException, AWTException {
-        Robot robot = new Robot();
-        String device = rawCommand.getString("device");
-        if(device.equals("keyboard")){
-            robot.keyPress(VK_WINDOWS);
-        }
+    public String executeCommand(JSONObject rawCommand){
+        try {
+            Robot robot = new Robot();
+            String device = rawCommand.getString("device");
+            String output = "";
+            if (device.equals("keyboard")) {
+                String command = rawCommand.getString("command");
+                String[] commandParts = command.split("/");
 
-        return null;
+                for(String commandPart: commandParts){
+                    if(commandPart.length() == 1){
+                        robot.keyPress(commandPart.charAt(0));
+                        output = output + (commandPart.charAt(0) + "/");
+                    }else{
+                        switch (commandPart){
+                            case "WINDOWS":
+                                robot.keyPress(VK_WINDOWS);
+                                output = output + "WINDOWS/";
+                                break;
+                            case "WINDOWS-RELEASE":
+                                robot.keyRelease(VK_WINDOWS);
+                                output = output + "WINDOWS-RELEASE/";
+                                break;
+                            case "ENTER":
+                                robot.keyPress(VK_ENTER);
+                                output = output + "ENTER/";
+                                break;
+                        }
+                    }
+                }
+            }
+            return "success/"+output;
+        }catch(AWTException|JSONException e){
+            return e.getMessage();
+        }
     }
 
 }
