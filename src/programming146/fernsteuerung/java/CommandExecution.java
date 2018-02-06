d49f80b99efb00916d93b5feddb2b4794a70a21f
programmingflo <flori08mans@gmail.com>
2018-02-06 16:36:14 +0100

    add lower/upper case handling

--- src/programming146/fernsteuerung/java/CommandExecution.java
+++ src/programming146/fernsteuerung/java/CommandExecution.java
@@ -6,6 +6,7 @@ import org.json.JSONObject;
 import java.awt.*;
 
 import static java.awt.event.KeyEvent.*;
+import static java.lang.Character.isUpperCase;
 
 
 public class CommandExecution {
@@ -22,24 +23,29 @@ public class CommandExecution {
                 String[] commandParts = command.split("/");
 
                 for(String commandPart: commandParts){
-                    if(commandPart.length() == 1){
-                        robot.keyPress(commandPart.charAt(0));
-                        output = output + (commandPart.charAt(0) + "/");
-                    }else{
-                        switch (commandPart){
-                            case "WINDOWS":
-                                robot.keyPress(VK_WINDOWS);
-                                output = output + "WINDOWS/";
-                                break;
-                            case "WINDOWS-RELEASE":
-                                robot.keyRelease(VK_WINDOWS);
-                                output = output + "WINDOWS-RELEASE/";
-                                break;
-                            case "ENTER":
-                                robot.keyPress(VK_ENTER);
-                                output = output + "ENTER/";
-                                break;
-                        }
+                    switch (commandPart){
+                        case "WINDOWS":
+                            robot.keyPress(VK_WINDOWS);
+                            output = output + "WINDOWS/";
+                            break;
+                        case "WINDOWS-RELEASE":
+                            robot.keyRelease(VK_WINDOWS);
+                            output = output + "WINDOWS-RELEASE/";
+                            break;
+                        case "ENTER":
+                            robot.keyPress(VK_ENTER);
+                            output = output + "ENTER/";
+                            break;
+                        default:
+                            for(Integer i = 0; i <= commandPart.length()-1; i++){
+                                if(isUpperCase(commandPart.charAt(i))){
+                                    robot.keyPress(VK_SHIFT);
+                                }
+                                robot.keyPress(commandPart.charAt(i));
+                                if(isUpperCase(commandPart.charAt(i))){
+                                    robot.keyRelease(VK_SHIFT);
+                                }
+                            }
                     }
                 }
             }
